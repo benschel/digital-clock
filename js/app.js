38e89ad8d6bb20013b9d29afbb5a3b599fb482dd
Ben <benschel170@gmail.com>
2015-03-01 04:39:04 -0500

    simplified js

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
         if (number === null) {
             return;
         }
-        
+
         var matrix = numberData[number];
 
         for (i = 0; i < matrix.length; i++) {
@@ -36,24 +36,11 @@ $(document).ready(function() {
         return number.toString().split('').map(Number);
     };
 
-    if (hour > 9) {
-        hourDigit1 = splitNumber(hour)[0];
-        hourDigit2 = splitNumber(hour)[1];
-    } else if (hour === 0) {
-        hourDigit1 = 1;
-        hourDigit2 = 2;
-    } else {
-        hourDigit1 = null;
-        hourDigit2 = hour;
-    }
+    hourDigit1 = hour > 9 || hour === 0 ? 1 : null;
+    hourDigit2 = hour > 9 ? hour - 10 : (hour === 0 ? 2 : hour);
 
-    if (minutes > 9) {
-        minDigit1 = splitNumber(minutes)[0];
-        minDigit2 = splitNumber(minutes)[1];
-    } else {
-        minDigit1 = 0;
-        minDigit2 = minutes;
-    }
+    minDigit1 = minutes > 9 ? splitNumber(minutes)[0] : 0;
+    minDigit2 = minutes > 9 ? splitNumber(minutes)[1] : minutes;
 
     renderNumber(hourDigit1, 'hour-1');
     renderNumber(hourDigit2, 'hour-2');
